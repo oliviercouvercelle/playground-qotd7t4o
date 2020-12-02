0cf467d898edb2318fc888f7a88fca7f2770cd13
oliviercouvercelel <olivier.couvercelle@gmail.com>
2020-12-02 21:32:16 +0100

    quelques modifications

--- python-project/Variables_et_fonctions/Programme_calcul_oliv.py
+++ python-project/Variables_et_fonctions/Programme_calcul_oliv.py
@@ -3,7 +3,15 @@ def mon_programme(n):
     # Les valeurs pour la variable en entrée seront automatiquement données
     # Ecrire ci-dessous en n'oubliant pas d'indenter
     x=n-3
-    pass
+    print(x)
+    y=x*2
+    print(y)
+    z=y**2
+    print(z)
+    a = z/10
+    print(a)
+    reponse=n+x+y+z+a
+    print(reponse)
     # Appuyer sur Run pour recevoir les instructions
  
     
